ebb9cb02de00234cd31cc72c5c403eb12e4fe523
ndifix <yoshiki.linuxmint@gmail.com>
2019-06-25 11:39:59 +0900

    Quoziente init

--- math.cpp
+++ math.cpp
@@ -72,3 +72,32 @@ long long combination(int n, int r){
   combination_memo[n][r]=combination(n-1,r)+combination(n-1,r-1);
   return combination_memo[n][r];
 }//combination_memo is needed
+
+class Quoziente{
+        private:
+    long long numer, denom;
+        public:
+    Quoziente(int n, int d){numer=n; denom=d;}
+    Quoziente(long long n, long long d){numer=n; denom=d;}
+    Quoziente(){numer=denom=1;}
+    int gcd(int a,int b){if(a<b)return gcd(b,a);if(a%b==0)return b;return gcd(b, a%b);}
+    void nomalize(){int div=gcd(numer,denom);numer/=div;denom/=div;}
+
+    void operator+=(int n){}
+    void operator-=(int n){}
+    void operator*=(int n){}
+    void operator/=(int n){}
+    Quoziente operator+(int n){}
+    Quoziente operator-(int n){}
+    Quoziente operator*(int n){}
+    Quoziente operator/(int n){}
+    void operator+=(Quoziente n){}
+    void operator-=(Quoziente n){}
+    void operator*=(Quoziente n){}
+    void operator/=(Quoziente n){}
+    Quoziente operator+(Quoziente n){}
+    Quoziente operator-(Quoziente n){}
+    Quoziente operator*(Quoziente n){}
+    Quoziente operator/(Quoziente n){}
+};
+
