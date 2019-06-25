a8719136aa21b38d3535ed59e36d33ede2ba592a
ndifix <yoshiki.linuxmint@gmail.com>
2019-06-25 17:00:37 +0900

    lower/upper bound added

--- utility.cpp
+++ utility.cpp
@@ -27,3 +27,24 @@ std::map<int,int> mapMerge(std::map<int,int> A, std::map<int,int> B){
     return ret;
 }
 template<class T>void sort(T &v){std::sort(v.begin(),v.end());}
+template<class T>int lower_bound(std::vector<T> &a, T key){
+	int s=0,e=a.size();
+	int mid;
+	while(e-s>2){
+		mid=(e+s)/2;
+		if(a[mid] < key)s=mid;
+		else if(a[mid] > key)e=mid;
+		else if(a[mid]==key)e=mid+1;
+	}
+	if(a[s]>=key)return s;if(a[s+1]>=key)return s+1;return s+2;
+}
+template<class T>int upper_bound(std::vector<T> &a, T key){
+	int s=0,e=a.size();
+	int mid;
+	while(e-s>2){
+		mid=(e+s)/2;
+		if(a[mid]<=key)s=mid;
+		else if(a[mid]>key && a[mid+1]>key)e=mid+1;
+	}
+	if(a[s]>key)return s;if(a[s+1]>key)return s+1;return s+2;
+}
