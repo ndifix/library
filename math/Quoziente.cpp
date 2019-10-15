9d909685c50834c7243cb27501b9ce2c0efa88f0
ndifix <yoshiki.linuxmint@gmail.com>
2019-10-15 22:17:54 +0900

    有理数class

---
+++ math/Quoziente.cpp
@@ -0,0 +1,27 @@
+class Quoziente{
+        private:
+    long long numer, denom;
+        public:
+    Quoziente(int n, int d){numer=n; denom=d;}
+    Quoziente(long long n, long long d){numer=n; denom=d;}
+    Quoziente(){numer=denom=1;}
+    int gcd(int a,int b){if(a<b)return gcd(b,a);if(a%b==0)return b;return gcd(b, a%b);}
+    void nomalize(){int div=gcd(numer,denom);numer/=div;denom/=div;}
+
+    void operator+=(int n){}
+    void operator-=(int n){}
+    void operator*=(int n){}
+    void operator/=(int n){}
+    Quoziente operator+(int n){}
+    Quoziente operator-(int n){}
+    Quoziente operator*(int n){}
+    Quoziente operator/(int n){}
+    void operator+=(Quoziente n){}
+    void operator-=(Quoziente n){}
+    void operator*=(Quoziente n){}
+    void operator/=(Quoziente n){}
+    Quoziente operator+(Quoziente n){}
+    Quoziente operator-(Quoziente n){}
+    Quoziente operator*(Quoziente n){}
+    Quoziente operator/(Quoziente n){}
+};
