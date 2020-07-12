9c824218336b8fad57b2a51ed822723caae24c78
ndifix <yoshiki.linuxmint@gmail.com>
2020-07-12 23:32:06 +0900

    sqMatrixクラスを削除。Matrixクラスだけにした。

--- math/Matrix.cpp
+++ math/Matrix.cpp
@@ -5,7 +5,9 @@ namespace ndifix {
 template <class T>
 class Matrix {
  private:
+  int DefaultSize = 2;
   int row, collumn;
+  std::vector<std::vector<T>> data;
   void push_row(std::vector<int> v) {
     if (v.size() == collumn) data.push_back(v);
   }
@@ -14,10 +16,6 @@ class Matrix {
       for (int i = 0; i < row; i++) data[i].push_back(v[i]);
     }
   }
-
- protected:
-  int DefaultSize = 2;
-  std::vector<std::vector<T>> data;
 
  public:
 #pragma region Constructors
@@ -122,6 +120,12 @@ class Matrix {
 
 #pragma region Methods
 
+  bool isSquare() { return row == collumn; }
+
+  T det() {}
+
+  Matrix<T> invert() {}
+
   template <typename U>
   friend Matrix<U> kronecker(Matrix<U> &, Matrix<U> &);
 
@@ -139,6 +143,7 @@ std::ostream &operator<<(std::ostream &os, const Matrix<T> &m) {
   }
   return os;
 }
+
 template <class T>
 Matrix<T> kronecker(Matrix<T> &a, Matrix<T> &b) {
   ndifix::Matrix<T> ret(a.row * b.row, a.collumn * b.collumn);
@@ -148,76 +153,6 @@ Matrix<T> kronecker(Matrix<T> &a, Matrix<T> &b) {
       for (int br = 0; br < b.collumn; br++) {
         for (int bc = 0; bc < b.collumn; bc++) {
           ret.data[ar * a.row + br][ac * a.collumn + bc] = m * b.data[br][bc];
-        }
-      }
-    }
-  }
-  return ret;
-}
-
-template <class T>
-class sqMatrix : public Matrix<T> {
- private:
-  int size;
-
- public:
-#pragma region Constructors
-
-  sqMatrix() {
-    size = this->DefaultSize;
-    this->data.resize(size);
-    for (int i = 0; i < size; i++) this->data[i].resize(size);
-  }
-  sqMatrix(int s) {
-    if (s < 0) throw std::invalid_argument("行列のサイズが負になっています。");
-    size = s;
-    this->data.resize(size);
-    for (int i = 0; i < size; i++) this->data[i].resize(size);
-  }
-  sqMatrix(std::vector<std::vector<T>> &v) {
-    size = v.size();
-    for (int i = 0; i < size; i++) {
-      if (v[i].size() != size) {
-        throw std::invalid_argument("正方行列になっていません。");
-      }
-    }
-    this->data = v;
-  }
-
-#pragma endregion
-
-#pragma region Methods
-
-  T det() {}
-  sqMatrix invert() {}
-  template <typename U>
-  friend std::ostream &operator<<(std::ostream &os, const sqMatrix<U> &m);
-  template <typename U>
-  friend sqMatrix<U> tensor(sqMatrix<U> &, sqMatrix<U> &);
-
-#pragma endregion
-};  // end of sqMatrix
-
-template <typename U>
-std::ostream &operator<<(std::ostream &os, const sqMatrix<U> &m) {
-  for (int i = 0; i < m.size; i++) {
-    for (int j = 0; j < m.size; j++) {
-      os << m.data[i][j];
-      if (j != m.size - 1) os << "\t";
-      if (i != m.size - 1 && j == m.size - 1) os << "\n";
-    }
-  }
-  return os;
-}
-template <typename U>
-sqMatrix<U> tensor(ndifix::sqMatrix<U> &a, sqMatrix<U> &b) {
-  sqMatrix<U> ret(a.size * b.size);
-  for (int ar = 0; ar < a.size; ar++) {
-    for (int ac = 0; ac < a.size; ac++) {
-      int m = a.data[ar][ac];
-      for (int br = 0; br < b.size; br++) {
-        for (int bc = 0; bc < b.size; bc++) {
-          ret.data[ar * a.size + br][ac * a.size + bc] = m * b.data[br][bc];
         }
       }
     }
