85eda53040d44a56999209eeb99d2ebab3f3b55b
ndifix <yoshiki.linuxmint@gmail.com>
2020-07-21 03:15:41 +0900

    UDU分解を追加した

--- math/Calculus.cpp
+++ math/Calculus.cpp
@@ -1,3 +1,5 @@
+#include "Matrix.cpp"
+
 namespace ndifix {
 
 // ∫[a,b]f(x)dx を計算します。
@@ -11,4 +13,37 @@ double Integral(double (*f)(double), double a, double b) {
   }
   return ret;
 }
+
+// LDU分解をしてLを返します。
+Rmatrix LDU_L(Rmatrix &m) {
+  Rmatrix ret(m.Row(), m.Collumn());
+  for (int i = 0; i < m.Row(); i++) {
+    for (int j = 0; j < i; j++) {
+      ret[i][j] = m[i][j];
+    }
+  }
+  return ret;
+}
+
+// LDU分解をしてDを返します。
+Rmatrix LDU_D(Rmatrix &m) {
+  Rmatrix ret(m.Row(), m.Collumn());
+  for (int i = 0; i < m.Row(); i++) {
+    ret[i][i] = m[i][i];
+  }
+
+  return ret;
+}
+
+// LDU分解をしてUを返します。
+Rmatrix LDU_U(Rmatrix &m) {
+  Rmatrix ret(m.Row(), m.Collumn());
+  for (int i = 0; i < m.Row(); i++) {
+    for (int j = i + 1; j < m.Collumn(); j++) {
+      ret[i][j] = m[i][j];
+    }
+  }
+  return ret;
+}
+
 }  // namespace ndifix
